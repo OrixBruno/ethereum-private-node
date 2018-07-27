6d2e0eeefe1fd72b54a8c8dccd2de613f0107e42
bsnascimento <bsnascimento@brq.com>
2018-07-27 15:15:23 -0300

    Adicionando fit log

--- startgeth.sh
+++ startgeth.sh
@@ -8,4 +8,5 @@
 # ./geth  --rpc --networkid=39318 --maxpeers=0 --datadir=devChain  --rpccorsdomain "*" --rpcaddr "0.0.0.0" --rpcapi "eth,web3,personal,net,miner,admin"  --verbosity 0 console  
 
 # SUBIR GET MANUALMENTE
-docker-compose up -d --build geth
+docker-compose up -d --build geth \
+&& docker-compose logs -f
